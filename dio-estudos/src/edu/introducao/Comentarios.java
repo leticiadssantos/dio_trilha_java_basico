910cf468da75727dc230dd8e12adf9e27025b6d2
Letícia Santos <110691992+leticiadssantos@users.noreply.github.com>
2022-10-28 21:46:53 -0300

    Add files via upload

--- dio-estudos/src/edu/introducao/Comentarios.java
+++ dio-estudos/src/edu/introducao/Comentarios.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package edu.introducao;
 /** 
  * Estas duas estrelinhas acima
@@ -8,20 +8,8 @@ package edu.introducao;
  * Que incrível !!!
  */
 
-public class Comentarios {
-    
-}
-=======
-package edu.introducao;
-/** 
- * Estas duas estrelinhas acima
- * é para identificar que você
- * pretende elaborar um comentário
- * a nível de documentação.
- * Que incrível !!!
- */
+
 
 public class Comentarios {
     
 }
->>>>>>> 76a6ffb213bba201b4e1a7fba1ec10dcb4f41ce5
---
+++ dio-estudos/src/edu/introducao/Desafio.java
@@ -0,0 +1,31 @@
+package edu.introducao;
+
+import java.util.Scanner;
+
+public class Desafio {
+
+
+    public static void main(String[] args) {
+      
+      Scanner teclado = new Scanner(System.in);
+      
+     System.out.println("Digite um numero: ");
+     int a = teclado.nextInt();
+     
+      System.out.println("Digite um numero: ");
+     int b = teclado.nextInt();
+     
+     
+
+        if (a == b ) {
+          System.out.println("Sao iguais");
+        } else {
+          System.out.println("Nao sao iguais");
+        }
+        
+    
+
+    }
+}
+    
+
